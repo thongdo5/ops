86a5576c4a9b502190714b4bfce61762e851e59d
monthin <thongdo5@gmail.com>
2023-10-15 02:01:38 +0700

    create terraform to launch 3 instances

--- lab-docs/terraform/v2-ec2.tf
+++ lab-docs/terraform/v2-ec2.tf
@@ -2,16 +2,23 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_instance" "demo-server" {
-  ami             = "ami-00b89387e88d35b2b"
-  instance_type   = "t2.micro"
-  key_name        = "demo-server"
-  security_groups = ["demo-sg"]
+resource "aws_instance" "ei-server" {
+  ami           = "ami-0d02292614a3b0df1"
+  instance_type = "t2.micro"
+  key_name      = "ei-server"
+  # security_groups = ["ei-sg"]
+  vpc_security_group_ids = [aws_security_group.ei-sg.id]
+  subnet_id              = aws_subnet.ei-apple-public-subnet.id
+  for_each               = toset(["jenkins-master", "build-slave", "ansible"])
+  tags = {
+    Name = "${each.key}"
+  }
 }
 
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
+resource "aws_security_group" "ei-sg" {
+  name        = "ei-sg"
   description = "SSH access"
+  vpc_id      = aws_vpc.ei-vpc.id
 
   ingress {
     description = "SSH access"
@@ -32,4 +39,56 @@ resource "aws_security_group" "demo-sg" {
   tags = {
     Name = "ssh-pro"
   }
+}
+
+resource "aws_vpc" "ei-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "ei-vpc"
+  }
+}
+
+resource "aws_subnet" "ei-apple-public-subnet" {
+  vpc_id                  = aws_vpc.ei-vpc.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "ap-southeast-2a"
+  tags = {
+    Name = "ei-apple-public-subnet"
+  }
+}
+
+resource "aws_subnet" "ei-melon-public-subnet" {
+  vpc_id                  = aws_vpc.ei-vpc.id
+  cidr_block              = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "ap-southeast-2b"
+  tags = {
+    Name = "ei-melon-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "ei-igw" {
+  vpc_id = aws_vpc.ei-vpc.id
+  tags = {
+    Name = "ei-igw"
+  }
+}
+
+resource "aws_route_table" "ei-public-rt" {
+  vpc_id = aws_vpc.ei-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ei-igw.id
+  }
+}
+
+resource "aws_route_table_association" "ei-apple-rta-public-subnet" {
+  subnet_id      = aws_subnet.ei-apple-public-subnet.id
+  route_table_id = aws_route_table.ei-public-rt.id
+}
+
+resource "aws_route_table_association" "ei-melon-rta-public-subnet" {
+  subnet_id      = aws_subnet.ei-melon-public-subnet.id
+  route_table_id = aws_route_table.ei-public-rt.id
 }
